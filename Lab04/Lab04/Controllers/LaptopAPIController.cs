0a4438291d14143a89ec93b0a32e8106f9a926ff
= <=>
2020-11-05 11:52:40 +0700

    update

--- Lab04/Lab04/Controllers/LaptopAPIController.cs
+++ Lab04/Lab04/Controllers/LaptopAPIController.cs
@@ -12,99 +12,122 @@ namespace Lab04.Controllers
 {
     public class LaptopAPIController : ApiController
     {
-        FilterDbContext db = new FilterDbContext();
-        public IEnumerable<laptop> getAll()
+        
+        //public IEnumerable<laptop> getAll()
+        //{
+        //    var searchQuery = db.laptops;
+        //    return searchQuery;
+        //}
+        //public IHttpActionResult GetProduct(string features_hash)
+        //{
+        //    SearchParameters seacrhParameters = new SearchParameters();
+        //    string[] ListQuery;
+        //    ListQuery = features_hash.Split('_');
+        //    foreach (var item in ListQuery)
+        //    {
+        //        string typeQuery = item.Split('-')[0];
+        //        string QueryData = item.Split('-')[1];
+        //        switch (typeQuery)
+        //        {
+        //            case "1":
+        //                seacrhParameters.SearchTerm = QueryData;
+        //                break;
+        //            case "2":
+        //                seacrhParameters.Company.Add(QueryData);
+        //                break;
+        //            case "3":
+        //                seacrhParameters.ScreenResolution.Add(QueryData);
+        //                break;
+        //            case "4":
+        //                seacrhParameters.TypeName.Add(QueryData);
+        //                break;
+        //            case "5":
+        //                seacrhParameters.Inches.Add(float.Parse(QueryData));
+        //                break;
+        //            case "6":
+        //                seacrhParameters.CPU.Add(QueryData);
+        //                break;
+        //            case "7":
+        //                seacrhParameters.Speed.Add(float.Parse(QueryData));
+        //                break;
+        //            case "8":
+        //                seacrhParameters.Memory.Add(QueryData);
+        //                break;
+        //            case "9":
+        //                seacrhParameters.Ram.Add(Int32.Parse(QueryData));
+        //                break;
+        //            case "10":
+        //                seacrhParameters.GPU.Add(QueryData);
+        //                break;
+        //            case "11":
+        //                seacrhParameters.OS.Add(QueryData);
+        //                break;
+        //            case "12":
+        //                seacrhParameters.PriceLow = double.Parse(QueryData);
+        //                break;
+        //            case "13":
+        //                seacrhParameters.PriceHigh = double.Parse(QueryData);
+        //                break;
+        //            case "14":
+        //                seacrhParameters.Weight.Add(float.Parse(QueryData));
+        //                break;
+        //            case "15":
+        //                switch (QueryData)
+        //                {
+        //                    case "1":
+        //                        seacrhParameters.SortBy = SortCriteria.PriceHighToLow;
+        //                        break;
+        //                    case "2":
+        //                        seacrhParameters.SortBy = SortCriteria.PriceLowToHigh;
+        //                        break;
+        //                    default:
+        //                        seacrhParameters.SortBy = SortCriteria.Relevance;
+        //                        break;
+        //                }
+        //                break;
+        //            default: 
+        //                break;
+        //        }
+        //    }
+        //    var searchQuery = new SearchBuilder().
+        //    SetSearchTerm(seacrhParameters.SearchTerm).//1 
+        //    SetCompany(seacrhParameters.Company).//2 
+        //    SetScreenResolution(seacrhParameters.ScreenResolution).//3 
+        //    SetTypeName(seacrhParameters.TypeName).//4 
+        //    SetInch(seacrhParameters.Inches).//5 
+        //    SetCPU(seacrhParameters.CPU).//6 
+        //    SetSpeed(seacrhParameters.Speed).//7 
+        //    SetMemory(seacrhParameters.Memory).//8 
+        //    SetRam(seacrhParameters.Ram).//9 
+        //    SetGPU(seacrhParameters.GPU).//10 
+        //    SetOS(seacrhParameters.OS).//11 
+        //    SetPriceLow(seacrhParameters.PriceLow).//12 
+        //    SetPriceHigh(seacrhParameters.PriceHigh).//13 
+        //    SetWeight(seacrhParameters.Weight)//14 
+        //    .SetSortBy(SortCriteria.PriceHighToLow)//15 
+        //    .Build(db);
+        //    return Ok(searchQuery);
+        //}
+        public IHttpActionResult Search(SearchParameters searchParameters)
         {
-            var searchQuery = db.laptops;
-            return searchQuery;
-        }
-        public IHttpActionResult GetProduct(string features_hash)
-        {
-            SearchParameters seacrhParameters = new SearchParameters();
-            string[] ListQuery;
-            ListQuery = features_hash.Split('_');
-            foreach (var item in ListQuery)
-            {
-                string typeQuery = item.Split('-')[0];
-                string QueryData = item.Split('-')[1];
-                switch (typeQuery)
-                {
-                    case "1":
-                        seacrhParameters.SearchTerm = QueryData;
-                        break;
-                    case "2":
-                        seacrhParameters.Company.Add(QueryData);
-                        break;
-                    case "3":
-                        seacrhParameters.ScreenResolution.Add(QueryData);
-                        break;
-                    case "4":
-                        seacrhParameters.TypeName.Add(QueryData);
-                        break;
-                    case "5":
-                        seacrhParameters.Inches.Add(float.Parse(QueryData));
-                        break;
-                    case "6":
-                        seacrhParameters.CPU.Add(QueryData);
-                        break;
-                    case "7":
-                        seacrhParameters.Speed.Add(float.Parse(QueryData));
-                        break;
-                    case "8":
-                        seacrhParameters.Memory.Add(QueryData);
-                        break;
-                    case "9":
-                        seacrhParameters.Ram.Add(Int32.Parse(QueryData));
-                        break;
-                    case "10":
-                        seacrhParameters.GPU.Add(QueryData);
-                        break;
-                    case "11":
-                        seacrhParameters.OS.Add(QueryData);
-                        break;
-                    case "12":
-                        seacrhParameters.PriceLow = double.Parse(QueryData);
-                        break;
-                    case "13":
-                        seacrhParameters.PriceHigh = double.Parse(QueryData);
-                        break;
-                    case "14":
-                        seacrhParameters.Weight.Add(float.Parse(QueryData));
-                        break;
-                    case "15":
-                        switch (QueryData)
-                        {
-                            case "1":
-                                seacrhParameters.SortBy = SortCriteria.PriceHighToLow;
-                                break;
-                            case "2":
-                                seacrhParameters.SortBy = SortCriteria.PriceLowToHigh;
-                                break;
-                            default:
-                                seacrhParameters.SortBy = SortCriteria.Relevance;
-                                break;
-                        }
-                        break;
-                    default: 
-                        break;
-                }
-            }
+            FilterDbContext db = new FilterDbContext();
+
             var searchQuery = new SearchBuilder().
-            SetSearchTerm(seacrhParameters.SearchTerm).//1 
-            SetCompany(seacrhParameters.Company).//2 
-            SetScreenResolution(seacrhParameters.ScreenResolution).//3 
-            SetTypeName(seacrhParameters.TypeName).//4 
-            SetInch(seacrhParameters.Inches).//5 
-            SetCPU(seacrhParameters.CPU).//6 
-            SetSpeed(seacrhParameters.Speed).//7 
-            SetMemory(seacrhParameters.Memory).//8 
-            SetRam(seacrhParameters.Ram).//9 
-            SetGPU(seacrhParameters.GPU).//10 
-            SetOS(seacrhParameters.OS).//11 
-            SetPriceLow(seacrhParameters.PriceLow).//12 
-            SetPriceHigh(seacrhParameters.PriceHigh).//13 
-            SetWeight(seacrhParameters.Weight)//14 
-            .SetSortBy(SortCriteria.PriceHighToLow)//15 
+            SetSearchTerm(searchParameters.SearchTerm).
+            SetCompany(searchParameters.Company).
+            SetScreenResolution(searchParameters.ScreenResolution).
+            SetTypeName(searchParameters.TypeName).
+            SetInch(searchParameters.Inches).
+            SetCPU(searchParameters.CPU). 
+            SetSpeed(searchParameters.Speed).
+            SetMemory(searchParameters.Memory).
+            SetRam(searchParameters.Ram).
+            SetGPU(searchParameters.GPU).
+            SetOS(searchParameters.OS). 
+            SetPriceLow(searchParameters.PriceLow).
+            SetPriceHigh(searchParameters.PriceHigh). 
+            SetWeight(searchParameters.Weight)
+            .SetSortBy(SortCriteria.PriceHighToLow)
             .Build(db);
             return Ok(searchQuery);
         }
--- Lab04/Lab04/Controllers/LaptopController.cs
+++ Lab04/Lab04/Controllers/LaptopController.cs
@@ -1,4 +1,5 @@
-﻿using Lab04.Models;
+﻿using Lab04.Helper;
+using Lab04.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,20 +16,55 @@ namespace Lab04.Controllers
         {
             return View();
         }
-        public ActionResult getCompany()
+        public ActionResult GetSearchFilter()
         {
-            var ListCompany = db.laptops.GroupBy(l => l.Company).Select(l => l.Key);
-            return PartialView(ListCompany);
+            SearchParameters search = new SearchParameters();
+            var listlaptop = db.laptops.ToList();
+            //company
+            search.Company          = listlaptop.GroupBy(l => l.Company).Select(l => l.Key).ToList();
+            //CPU
+            search.CPU              = listlaptop.GroupBy(l => l.Cpu).Select(l => l.Key).ToList();
+            //type name
+            search.TypeName         = listlaptop.GroupBy(l => l.TypeName).Select(l => l.Key).ToList();
+            //Ram
+            search.Memory           = listlaptop.GroupBy(l => l.Memory).Select(l => l.Key).ToList();
+            //OS
+            search.OS               = listlaptop.GroupBy(l => l.OpSys).Select(l => l.Key).ToList();
+            //ScreenResolution
+            search.ScreenResolution = listlaptop.GroupBy(l => l.ScreenResolution).Select(l => l.Key).ToList();
+            //speed
+            //search.Speed            = listlaptop.GroupBy(l => l.Speed).Select(l => l.Key).ToList();
+            //Wright
+            //search.Weight           = listlaptop.GroupBy(l => l.Weight).Select(l => l.Key).ToList();
+            //PriceHigh
+            //search.PriceHigh      = listlaptop.GroupBy(l => l.Price_euros).Select(l => l.Key).ToList();
+
+            return PartialView(search);
         }
-        public ActionResult getCPU()
-        {
-            var ListCPU = db.laptops.GroupBy(l => l.Cpu).Select(l => l.Key);
-            return PartialView(ListCPU);
-        }
-        public ActionResult getRam()
-        {
-            var ListRam = db.laptops.GroupBy(l => l.Ram).Select(l => l.Key);
-            return PartialView(ListRam);
-        }
+        ////Company
+        //public ActionResult getCompany()
+        //{
+        //    var ListCompany = db.laptops.GroupBy(l => l.Company).Select(l => l.Key);
+        //    return PartialView(ListCompany);
+        //}
+        ////CPU
+        //public ActionResult getCPU()
+        //{
+        //    var ListCPU = db.laptops.GroupBy(l => l.Cpu).Select(l => l.Key);
+        //    return PartialView(ListCPU);
+        //}
+        ////Ram
+        //public ActionResult getRam()
+        //{
+        //    var ListRam = db.laptops.GroupBy(l => l.Ram).Select(l => l.Key);
+        //    return PartialView(ListRam);
+        //}
+        ////Inches
+        //public ActionResult getInches()
+        //{
+        //    var ListInches = db.laptops.GroupBy(l => l.Inches).Select(l => l.Key);
+        //    return PartialView(ListInches);
+        //}
+
     }
 }
